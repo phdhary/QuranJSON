80ddaf1079a30d05dd80d0ba4fa7022f16fceb56
Hary <phdhary@gmail.com>
2022-02-08 20:52:22 +0700

    remove unused fs

--- road_to_jannah.js
+++ road_to_jannah.js
@@ -2,10 +2,8 @@ const appRoutes = require("./router/router")
 const express = require('express')
 const app = express()
 const { PORT } = require("./constant")
-var fs = require('fs');
 
 app.use(appRoutes())
-
 
 app.listen(PORT, () => {
   console.log(`بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيم\nListening on port ${PORT}`)
