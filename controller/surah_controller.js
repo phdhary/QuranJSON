43221a628df8379b1031343f9061d470ead6bbda
Hary <phdhary@gmail.com>
2022-02-08 21:01:47 +0700

    add error desc

--- controller/surah_controller.js
+++ controller/surah_controller.js
@@ -4,7 +4,7 @@ async function surahController(req, res, next) {
     var surahId = req.params.surahId;
     fs.readFile(`surah/${surahId}.json`, 'utf8', (err, data) => {
         if (err) {
-            next(err)
+            next(Error(`Page not found in ${req.url}`))
         } else {
             res.send(JSON.parse(data))
         }
--- road_to_jannah.js
+++ road_to_jannah.js
@@ -5,6 +5,11 @@ const { PORT } = require("./constant")
 
 app.use(appRoutes())
 
+app.use((req, res, next) => {
+  res.status(404)
+  next(Error(`The page not found in path ${req.url} and method ${req.method}`))
+})
+
 app.listen(PORT, () => {
   console.log(`بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيم\nListening on port ${PORT}`)
 })
